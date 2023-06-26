4b467a2c22c28f1b8022db99c5c7244fbaa5f9ad
ViktoriiaOleksiienko <137415344+ViktoriiaOleksiienko@users.noreply.github.com>
2023-06-26 14:00:57 +0300

    homework 1 (added task1 and task2)

---
+++ aqa_project/hw1/task1.py
@@ -0,0 +1,14 @@
+first_name = "Viktoriia"
+second_name = "Oleksiienko"
+full_name = first_name + ' ' + second_name
+print(full_name)
+
+print(full_name.lower())
+print(full_name.upper())
+print(first_name.capitalize() + ' ' + second_name.capitalize())
+
+first_name = "   Viktoriia"
+second_name = "Oleksiienko   "
+print('\tViktoriia')
+print('Viktoriia\nOleksiienko')
+print(full_name.strip())
---
+++ aqa_project/hw1/task2.py
@@ -0,0 +1,4 @@
+course = 36.5686
+dollar = 1000 / course
+print(round(dollar, 2))
+print(f'Поточний курс складає: {course}')
