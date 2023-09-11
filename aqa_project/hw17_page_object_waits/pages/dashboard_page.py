693bc92617c0ecfc686bcd540f3f64c7549cd37e
ViktoriiaOleksiienko <137415344+ViktoriiaOleksiienko@users.noreply.github.com>
2023-09-11 15:41:45 +0300

    homework 17 (added edited tests)

--- aqa_project/hw17_page_object_waits/pages/dashboard_page.py
+++ aqa_project/hw17_page_object_waits/pages/dashboard_page.py
@@ -1,7 +1,5 @@
 import time
 from hw17_page_object_waits.pages.base_page import BasePage
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 class Dashboard(BasePage):
@@ -9,27 +7,29 @@ class Dashboard(BasePage):
         super().__init__(driver)
 
     def go_to_discounts(self):
-        locator = ('xpath', '//*[@id="home"]/div/div[2]/div[1]/div[2]/div/ul/li[1]/a')
-        self.click_to_element(locator)
+        discounts_button_locator = ('xpath', '//*[@id="home"]/div/div[2]/div[1]/div[2]/div/ul/li[1]/a')
+        discounts_page_locator = ('xpath', '//*[@id="viewport"]/div[8]')
+        self.wait_until_element_appears(discounts_page_locator)
+        self.click_to_element(discounts_button_locator)
 
     def go_to_login_form(self):
         login_button_locator = ('xpath', '//*[@id="site-header"]/div/div/div[3]/button[2]')
-        self.click_to_element(login_button_locator)
         modal_button_locator = ('xpath', '//*[@id="viewport"]/div[8]/div[2]/button')
-        WebDriverWait(self._driver, 10).until(EC.element_to_be_clickable(('xpath', '//*[@id="viewport"]/div[8]/div[2]/button')))
+        self.click_to_element(login_button_locator)
+        self.wait_until_element_clicked(modal_button_locator)
         self.click_to_element(modal_button_locator)
 
     def go_to_basket(self):
         basket_button_locator = ('xpath', '//*[@id="site-header"]/div/div/div[3]/button[1]')
-        self.click_to_element(basket_button_locator)
         modal_button_locator = ('xpath', '//*[@id="viewport"]/div[2]/div/div[1]/button')
-        WebDriverWait(self._driver, 10).until(EC.element_to_be_clickable(('xpath', '//*[@id="viewport"]/div[2]/div/div[1]/button')))
+        self.click_to_element(basket_button_locator)
+        self.wait_until_element_clicked(modal_button_locator)
         self.click_to_element(modal_button_locator)
 
     def search_book1(self, message):
         field_locator = ('xpath', '//*[@id="search"]')
-        self.send_keys_to_element(field_locator, message)
         search_locator = ('xpath', '//*[@id="site-header"]/div/div/div[2]/div[1]/div/button')
+        self.send_keys_to_element(field_locator, message)
         self.click_to_element(search_locator)
 
     def search_book2(self, message):
--- aqa_project/hw17_page_object_waits/tests/test_dashboard_page.py
+++ aqa_project/hw17_page_object_waits/tests/test_dashboard_page.py
@@ -1,21 +1,16 @@
 import time
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_go_to_discounts(dashboard):
     dashboard.go_to_discounts()
-    WebDriverWait(dashboard._driver, 10).until(EC.url_contains('https://www.yakaboo.ua/ua/promotions'))
 
 
 def test_go_to_login_form(dashboard):
     dashboard.go_to_login_form()
-    WebDriverWait(dashboard._driver, 10).until(EC.presence_of_element_located(('xpath', '//*[@id="viewport"]/div[8]/div[2]/div')))
 
 
 def test_go_to_basket(dashboard):
     dashboard.go_to_basket()
-    WebDriverWait(dashboard._driver, 10).until(EC.presence_of_element_located(('xpath', '//*[@id="viewport"]/div[2]/div/div[1]/button')))
 
 
 def test_search_book1(dashboard):
